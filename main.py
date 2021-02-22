057884028488bf701a4a82640c73ef143e398b4f
aliprf <aliprf@gmail.com>
2021-02-21 22:37:15 -0700

    train cofw

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     trainer_efn = TrainEfn(dataset_name=DatasetName.ds_cofw, use_augmented=True)
     # trainer_efn = TrainEfn(dataset_name=DatasetName.ds_wflw, use_augmented=True)
     # trainer_efn = TrainEfn(dataset_name=DatasetName.ds_300W, use_augmented=True)
-    trainer_efn.train(arch='efn', weight_path='last_efn.h5')
+    trainer_efn.train(arch='efn', weight_path='./models/last_efn.h5')
