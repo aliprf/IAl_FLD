9362d64e65902ab76c385774a394e5f470ac8045
aliprf <aliprf@gmail.com>
2021-03-01 13:10:00 -0700

    fg loss part : issue removed

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     # trainer = TrainHg(dataset_name=DatasetName.ds_cofw, use_augmented=True)
     # trainer = TrainHg(dataset_name=DatasetName.ds_wflw, use_augmented=True)
     trainer = TrainHg(dataset_name=DatasetName.ds_300W, use_augmented=True)
-    trainer.train(arch='hgNet', weight_path=None)
-    # trainer.train(arch='hgNet', weight_path='./models/last.h5')
+    # trainer.train(arch='hgNet', weight_path=None)
+    trainer.train(arch='hgNet', weight_path='./models/last.h5')
 
 
     '''train hourGlass'''
