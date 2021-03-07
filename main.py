155aa2631dcb578e756a2d0df8e65741d40f25ce
aliprf <aliprf@gmail.com>
2021-03-06 19:02:57 -0700

    HrNet - revised the loss function

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     # dlp.depict_weight_map_function(theta_0=theta_0, theta_1=theta_1)
     # dlp.depict_loss(theta_0=theta_0, theta_1=theta_1)
 
-    # trainer = TrainHg(dataset_name=DatasetName.ds_300W, use_augmented=True)
-    trainer = TrainHg(dataset_name=DatasetName.ds_cofw, use_augmented=True)
+    trainer = TrainHg(dataset_name=DatasetName.ds_300W, use_augmented=True)
+    # trainer = TrainHg(dataset_name=DatasetName.ds_cofw, use_augmented=True)
     # trainer = TrainHg(dataset_name=DatasetName.ds_wflw, use_augmented=True)
     # trainer.train(arch='hrnet', weight_path=None)
     trainer.train(arch='hrnet', weight_path='./models/last_hr.h5')
