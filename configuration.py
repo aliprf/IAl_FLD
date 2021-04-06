049c16705ace965facd0c41c7b518f8c6e74e6ca
aliprf <aliprf@gmail.com>
2021-04-05 20:23:35 -0600

    changing model

--- configuration.py
+++ configuration.py
@@ -15,7 +15,7 @@ class LearningConfig:
     Loss_threshold = 0.5
     Loss_threshold_2 = 0.02
 
-    virtual_batch_size = 500
+    virtual_batch_size = 250
 
     # batch_size = 3
     batch_size = 40
