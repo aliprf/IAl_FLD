5ae56541d29e22773ae9c1e257f949db56732733
aliprf <aliprf@gmail.com>
2021-04-12 10:13:42 -0600

    start creating model

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     # # # trainer.train(arch='hrnet', weight_path='./models/last_hr.h5', use_inter=True)
 
     '''train 1D models'''
-    trainer = Train1DNet(dataset_name=DatasetName.ds_cofw, use_augmented=True, multi_loss=True)
-    # trainer = Train1DNet(dataset_name=DatasetName.ds_wflw, use_augmented=True, multi_loss=True)
+    # trainer = Train1DNet(dataset_name=DatasetName.ds_cofw, use_augmented=True, multi_loss=True)
+    trainer = Train1DNet(dataset_name=DatasetName.ds_wflw, use_augmented=True, multi_loss=True)
     # trainer = Train1DNet(dataset_name=DatasetName.ds_300W, use_augmented=True, multi_loss=True)
     trainer.train(arch='arch_1d_ml', weight_path='./models/last_1d.h5', old_arch=False)
     # trainer.train(arch='arch_1d_ml', weight_path=None, old_arch=True)
