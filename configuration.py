6762f9fde3d0e826c74c1f691a8a6e86ddc0aed5
aliprf <aliprf@gmail.com>
2021-04-21 17:57:42 -0600

    self-attention modef

--- configuration.py
+++ configuration.py
@@ -15,10 +15,10 @@ class LearningConfig:
     Loss_threshold = 0.5
     Loss_threshold_2 = 0.02
 
-    virtual_batch_size = 250
+    virtual_batch_size = 100
 
     # batch_size = 3
-    batch_size = 20
+    batch_size = 50
     epochs = 1500
 
 
